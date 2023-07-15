840cef42033727bfd5f19ed6e463fc0c47782e70
EmunaTeller <78487762+EmunaTeller@users.noreply.github.com>
2023-07-16 01:20:27 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using GiphyWithDotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace GiphyWithDotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string _GiphyApiKey = "QsfRn4Ng0eSY2TwgXJvb3s0y7TBmJN45";
+        private readonly HttpClient _httpClient;
+        private readonly IMemoryCache _cache;
+        private readonly GifViewModel _gifViewModel;
+
+        public HomeController(IMemoryCache cache)
+        {
+            _httpClient = new HttpClient();
+            _gifViewModel = new GifViewModel();
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }        
+
+        public async Task<ActionResult> TrendingGifs()
+        {
+            if (_cache.TryGetValue("trendingGifs", out GifViewModel cachedGifs))
+            {
+                return View(cachedGifs);
+            }
+
+            var giphyResponse = await _gifViewModel.GetTrendingGifs(_GiphyApiKey, _httpClient);
+
+
+            _cache.Set("trendingGifs", giphyResponse, DateTime.Today.AddDays(1));
+
+            return View(giphyResponse);
+        }
+        
+        public IActionResult SearchGifs()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Search(string searchWord)
+        {
+            GifViewModel cachedGifs;
+            if (_cache.TryGetValue(searchWord, out cachedGifs))
+            {
+                return View("SearchGifs", cachedGifs);
+            }
+
+            var giphyResponse = await _gifViewModel.SearchGifs(_GiphyApiKey, _httpClient, searchWord);
+            
+            _cache.Set(searchWord, giphyResponse, DateTime.Today.AddDays(1));
+
+            return View("SearchGifs", giphyResponse);
+        }
+    }
+}
---
+++ Models/GifViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace GiphyWithDotNet.Models
+{
+    public class GifViewModel
+    {
+        public class Gif
+        {
+            public string Url { get; set; }
+        }
+
+        public async Task<GifViewModel> GetTrendingGifs(string GiphyApiKey, HttpClient httpClient)
+        {
+            var response = await httpClient.GetAsync($"https://api.giphy.com/v1/gifs/trending?api_key={GiphyApiKey}&limit=3");
+            response.EnsureSuccessStatusCode();
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var giphyResponse = JsonConvert.DeserializeObject<GifViewModel>(jsonString);
+            return giphyResponse;
+        }
+
+        public async Task<GifViewModel> SearchGifs(string GiphyApiKey, HttpClient httpClient, string searchWord)
+        {
+            var encodedSearchWord = Uri.EscapeDataString(searchWord ?? "");
+            var response = await httpClient.GetAsync($"https://api.giphy.com/v1/gifs/search?q={encodedSearchWord}&api_key={GiphyApiKey}&limit=3");
+            response.EnsureSuccessStatusCode();
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var giphyResponse = JsonConvert.DeserializeObject<GifViewModel>(jsonString);
+            return giphyResponse;
+        }
+    }
+}
